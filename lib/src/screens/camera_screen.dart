051a47ad4f3a4847a99524f7b29df18257ebc2e2
User <user@example.com>
2026-08-02 20:35:45 +0000

    Fix exhaustive switch error for Open Gate recording mode

--- lib/src/screens/camera_screen.dart
+++ lib/src/screens/camera_screen.dart
@@ -977,6 +977,7 @@ class _TopHud extends StatelessWidget {
   final CameraUiController controller;
 
   String get _format => switch (controller.recordingMode) {
+    RecordingMode.openGate => 'OPEN GATE',
     RecordingMode.uhd30 => 'UHD 16:9',
     RecordingMode.fhd30 => 'FHD 16:9',
     RecordingMode.fourThree30 => '1440p 4:3',

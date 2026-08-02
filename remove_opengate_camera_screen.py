e7a0106bdd4e2639a9f15d7190e2be9e411ec8f7
User <user@example.com>
2026-08-02 20:56:25 +0000

    Clean up scripts

--- remove_opengate_camera_screen.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('lib/src/screens/camera_screen.dart', 'r') as f:
-    content = f.read()
-
-content = content.replace("RecordingMode.openGate => 'OPEN GATE',\n    ", "")
-
-with open('lib/src/screens/camera_screen.dart', 'w') as f:
-    f.write(content)

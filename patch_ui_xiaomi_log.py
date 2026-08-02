25fd16d1962183d8c4afe08e5032b5776741c0ff
User <user@example.com>
2026-08-02 20:10:56 +0000

    Clean up scripts

--- patch_ui_xiaomi_log.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('lib/src/screens/settings_screen.dart', 'r') as f:
-    content = f.read()
-
-content = content.replace("ButtonSegment<bool>(value: true, label: Text('Zircon Log')),", "ButtonSegment<bool>(value: true, label: Text('Xiaomi Log')),")
-
-with open('lib/src/screens/settings_screen.dart', 'w') as f:
-    f.write(content)
